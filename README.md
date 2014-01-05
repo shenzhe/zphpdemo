b2f9ededf7b1e5916785a0307883d6d69feb5677
shenzhe <shenzhe163@gmail.com>
2014-01-05 18:52:42 +0800

    edit main.php

--- README.md
+++ README.md
@@ -10,6 +10,6 @@ zphpdemo
 
 集成了zphp相关的demo，每个demo内部有详细的readme
 
-所需的zphp框架git: https://github.com/shenzhe/zphp (git clone到根目录)
+所需的zphp框架git: https://github.com/shenzhe/zphp (git clone到此目录)
 
 
--- demo_composer/webroot/main.php
+++ demo_composer/webroot/main.php
@@ -1,9 +1,5 @@
 <?php
-
-require '../vendor/autoload.php';
-
 use ZPHP\ZPHP;
-
 $rootPath = dirname(__DIR__);
-
+require dirname(dirname($rootPath)) . DIRECTORY_SEPARATOR . 'zphp' . DIRECTORY_SEPARATOR . 'ZPHP' . DIRECTORY_SEPARATOR . 'ZPHP.php';
 ZPHP::run($rootPath);
