bda97c5a797849ee4b541c96b8bb98783a849192
shenzhe <shenzhe163@gmail.com>
2014-01-05 18:53:54 +0800

    debug composer

--- demo_composer/webroot/main.php
+++ demo_composer/webroot/main.php
@@ -1,5 +1,9 @@
 <?php
+
+require '../vendor/autoload.php';
+
 use ZPHP\ZPHP;
+
 $rootPath = dirname(__DIR__);
-require dirname(dirname($rootPath)) . DIRECTORY_SEPARATOR . 'zphp' . DIRECTORY_SEPARATOR . 'ZPHP' . DIRECTORY_SEPARATOR . 'ZPHP.php';
+
 ZPHP::run($rootPath);
