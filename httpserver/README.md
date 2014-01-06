a982b505f261025052ac4be86a2b2a521eb73230
半桶水 <shenzhe163@gmail.com>
2014-01-06 13:41:26 +0800

    debug readme

--- httpserver/README.md
+++ httpserver/README.md
@@ -7,4 +7,6 @@ zhttpserver Demo运行：
 
 3) 浏览器运行：http://host:8992
 
+4) 静态文件还是走nginx。需要在config/http/config.php里配置
+
 
--- httpserver/template/template/header.php
+++ httpserver/template/template/header.php
@@ -27,5 +27,5 @@
     <![endif]-->
 </head>
 <body>
-    <a href="https://github.com/shenzhe/zchat" target="_blank">
+    <a href="https://github.com/shenzhe/zphpdemo" target="_blank">
     <img style="position: absolute; top: 0; right: 0; border: 0; z-index:9999;" src="http://www.swoole.com/static/image/forkme_right_orange_ff7600.png" alt="Fork me on GitHub" /></a>
