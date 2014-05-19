12517c1cfde1275e6b4a5ae4f7d87720a81dfff8
shenzhe <shenzhe163@gmail.com>
2014-05-19 20:15:41 +0800

    debug config

--- todpole/config/default/config.php
+++ todpole/config/default/config.php
@@ -1,13 +1,15 @@
 <?php
 use ZPHP\ZPHP;
+define('PROJECT_NAME', 'todpole');
 $config =  array(
     'server_mode' => 'Http',
-    'project_name' => 'todpole',
+    'project_name' => PROJECT_NAME,
     'app_path' => 'apps',
     'ctrl_path' => 'ctrl',
     'project' => array(
         'log_path' => 'http',
-        'default_ctrl_name'=>'main'
+        'default_ctrl_name'=>'main',
+        'tpl_path' => ZPHP::getRootPath(). DS.'template'. DS . PROJECT_NAME.DS
     ),
 );
 return $config;
