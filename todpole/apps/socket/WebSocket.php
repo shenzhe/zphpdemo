d7db86ef298d5162cf94dafbbe6aadfd50964f81
半桶水 <shenzhe@shenzhedembp-2.lan>
2014-07-12 11:01:52 +0800

    add robot

--- todpole/apps/socket/WebSocket.php
+++ todpole/apps/socket/WebSocket.php
@@ -1,85 +1,127 @@
 <?php
 
 namespace socket;
+
 use ZPHP\Socket\Callback\WSServer;
 use ZPHP\Socket\Route;
 use ZPHP\Core\Config as ZConfig;
+use ZPHP\Conn\Factory as ZConn;
 
 class WebSocket extends WSServer
 {
-    public function wsOnOpen($fd, $data) 
+    public $num = 0;
+
+    public function getConnInfo($fd)
     {
-        echo "{$fd} connect success";
-        $uid = (substr(strval(microtime(true)), 6, 7)*100)%1000000;
-           if($uid<100000)
-           {
-               $uid += 100000; 
-           }
-           $this->send($fd, $data.pack("H*", '811e').'{"type":"welcome","id":'.$uid.'}', self::OPCODE_BINARY_FRAME);
-           $this->_ws[$fd]['uid'] = $uid;
+        $this->num++;
+        $info = $this->conn->getBuff($fd, 'info');
+        $this->log("get {$fd} info {$info} num:" . $this->num . " time:" . date("Y-m-d H:i:s"));
+        if (empty($info)) {
+            return array();
+        }
+        return \json_decode($info, true);
+    }
+
+    public function addConnInfo($fd, array $data)
+    {
+        $this->log("{$fd} addinfo");
+        $info = $this->getConnInfo($fd);
+        foreach ($data as $key => $val) {
+            $info[$key] = $val;
+        }
+        $this->conn->setBuff($fd, \json_encode($info), 'info');
+    }
+
+    public function wsOnOpen($fd, $data)
+    {
+        echo "{$fd} connect success\n";
+        $uid = (substr(strval(microtime(true)), 6, 7) * 100) % 1000000;
+        if ($uid < 100000) {
+            $uid += 100000;
+        }
+        $this->send($fd, $data . pack("H*", '811e') . '{"type":"welcome","id":' . $uid . '}', self::OPCODE_BINARY_FRAME);
+        $this->conn->addFd($fd, $uid);
+        $this->conn->add($uid, $fd);
     }
 
     public function wsOnClose($fd)
     {
-        $this->sendAll($fd, json_encode(array('type'=>'closed', 'id'=>$this->_ws[$fd]['uid'])));
+        //echo "{$fd} close\n";
+        //print_r($this->_ws);
+        //$this->sendAll($fd, json_encode(array('type'=>'closed', 'id'=>$this->_ws[$fd]['uid'])));
+        $uid = $this->conn->getUid($fd);
+        if (empty($uid)) return;
+        $this->sendAll($fd, json_encode(array('type' => 'closed', 'id' => $uid)));
+        $this->conn->delete($fd, $uid);
     }
 
-    
+
     public function sendAll($fd, $data)
     {
-        foreach ( $this->_ws as $clid => $info )
-        {
-            //if( $fd != $clid )
-            //{
-                $this->send( $clid , $data);
-            //}
+        $channelList = $this->conn->getChannel();
+        if (!empty($channelList)) {
+            foreach ($channelList as $clid) {
+                $ret = $this->send($clid, $data);
+                if(!$ret) {
+                    $this->log("{$clid} send error");
+                }
+            }
         }
-        
     }
 
 
     /**
-    * 接收到消息时
-    * @see WSProtocol::onSend()
-    */
+     * 接收到消息时
+     * @see WSProtocol::onSend()
+     */
     public function wsOnMessage($fd, $ws)
     {
-        $this->log("onSend: ".$ws['message']);
-        $message_data = json_decode( $ws['message'] , true );
-        if(!$message_data)
-        {
-            return ;
+        //$this->log("onSend: ".$ws['message']);
+        $message_data = json_decode($ws['message'], true);
+        if (!$message_data) {
+            return;
         }
-        
-        switch($message_data['type'])
-        {
+
+        $uid = $this->conn->getUid($fd);
+        switch ($message_data['type']) {
             // 更新用户
             case 'update':
                 // 转播给所有用户
                 $this->sendAll($fd, json_encode(
-                        array(
-                                'type'     => 'update',
-                                'id'         => $this->_ws[$fd]['uid'],
-                                'angle'   => $message_data["angle"]+0,
-                                'momentum' => $message_data["momentum"]+0,
-                                'x'                   => $message_data["x"]+0,
-                                'y'                   => $message_data["y"]+0,
-                                'life'                => 1,
-                                'name'           => isset($message_data['name']) ? $message_data['name'] : 'Guest.'.$this->_ws[$fd]['uid'],
-                                'authorized'  => false,
-                                )
-                        ));
+                    array(
+                        'type' => 'update',
+                        'id' => $uid,
+                        'angle' => $message_data["angle"] + 0,
+                        'momentum' => $message_data["momentum"] + 0,
+                        'x' => $message_data["x"] + 0,
+                        'y' => $message_data["y"] + 0,
+                        'life' => 1,
+                        'name' => isset($message_data['name']) ? $message_data['name'] : 'Guest.' . $uid,
+                        'authorized' => false,
+                    )
+                ));
                 return;
             // 聊天
             case 'message':
                 // 向大家说
                 $new_message = array(
-                    'type'=>'message', 
-                    'id'=>$this->_ws[$fd]['uid'],
-                    'message'=>$message_data['message'],
+                    'type' => 'message',
+                    'id' => $uid,
+                    'message' => $message_data['message'],
                 );
                 return $this->sendAll($fd, json_encode($new_message));
         }
+    }
+
+    public function onWorkerError()
+    {
+        $params = func_get_args();
+        echo "exit: {$params[3]}==============" . PHP_EOL;
+    }
+
+    public function log($msg)
+    {
+        //echo $msg.PHP_EOL;
     }
 
 }
---
+++ todpole/robot.php
@@ -0,0 +1,59 @@
+<?php
+require "zphp/ZPHP/Common/WebSocketClient.php";
+function getXY($id)
+{
+    return [
+        'angle' => "-4.734",
+        'momentum' => "0.000",
+        'name' => "Robot." . $id,
+        'type' => "update",
+        'x' => mt_rand(-300, 300),
+        'y' => mt_rand(-300, 300),
+    ];
+}
+
+$send_count = 0;
+$num = 100;  //机器人个数
+$host = '127.0.0.1';
+$prot = 8995;
+$clients = [];
+for ($i = 1; $i <= $num; $i++) {
+
+    $client = new Common\WebSocketClient($host, $prot);
+    $data = $client->connect();
+    $id = substr($data, -7, 6);
+    echo "{$i} : {$id}" . PHP_EOL;
+    $result = $client->send(json_encode(getXY($id)));
+    $fd = $client->getSocket()->sock;
+    $clients[$fd] = [$id, $client];
+    echo "robot {$fd} create success" . PHP_EOL;
+    usleep(20000);
+}
+echo "starttime: " . date("Y-m-d H:i:s") . PHP_EOL;
+$starttime = time();
+while (1) {
+    foreach ($clients as $index => $cli) {
+        $result = $cli[1]->send(json_encode(getXY($cli[0])));
+        $send_count++;
+        if ($result === false) {
+            unset($clients[$index]);
+            echo "{$cli[0]} error" . PHP_EOL;
+        } else {
+            $recvData = $cli[1]->recv();
+            if(!$recvData) {
+                unset($clients[$index]);
+                echo "{$index} recv error" . PHP_EOL;
+                return;
+            }
+
+        }
+    }
+    echo "send_count={$send_count}, etime: " . date("Y-m-d H:i:s") . PHP_EOL;
+    if (empty($clients)) {
+        echo "client empty, runing: " . (time() - $starttime) . PHP_EOL;
+        return;
+    }
+    sleep(1);
+}
+
+echo 'finish'.PHP_EOL;
