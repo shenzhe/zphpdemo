5fc0f541838f4130c6bb05c5cfdacaba8c223b62
shenzhe <shenzhe163@gmail.com>
2015-09-09 16:39:44 +0800

    todpole 配置

--- todpole/config/websocket/config.php
+++ todpole/config/websocket/config.php
@@ -12,7 +12,6 @@ return array(
         'log_path' => 'socket',
         'default_ctrl_name' => 'main',
         'static_url' => STATIC_URL,
-        'app_host' => '127.0.0.1:8991',
     ),
     'socket' => array(
         'host' => '0.0.0.0',                          //socket 监听ip
@@ -20,12 +19,12 @@ return array(
         'adapter' => 'Swoole',                          //socket 驱动模块
         'server_type' => Swoole::TYPE_WEBSOCKET,              //socket 业务模型 tcp/udp/http/websocket
         'protocol' => 'Json',                         //socket通信数据协议
-        'daemonize' => 0,                             //是否开启守护进程
+        'daemonize' => 1,                             //是否开启守护进程
         'client_class' => 'socket\\WebSocket',            //socket 回调类
         'work_mode' => 3,                             //工作模式：1：单进程单线程 2：多线程 3： 多进程
         'worker_num' => 4,                                 //工作进程数
         'task_worker_num' => 2,                                 //task进程数
         'max_request' => 0,                            //单个进程最大处理请求数
-        'debug_mode' => 1,                                  //打开调试模式
+        'debug_mode' => 0,                                  //打开调试模式
     ),
 );
